71797892efaf902e7b0de712eb6296d2569b5af1
tantality <angel.tretyak@mail.ru>
2023-12-09 22:25:46 +0300

    feat: add an auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from 'app/auth/auth.controller';
 
 @Module({
-  controllers: [],
+  controllers: [AuthController],
 })
 export class AppModule {}
