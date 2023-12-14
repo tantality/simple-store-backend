3700258942cb19fad1d8a1202985601be5e00d15
tantality <angel.tretyak@mail.ru>
2023-12-14 13:05:12 +0300

    feat: implement the receipt of a product by id

--- src/app/poducts/products.service.ts
+++ src/app/poducts/products.service.ts
@@ -4,4 +4,8 @@ import { ProductsRepo } from 'domain/repos/products.repo';
 @Injectable()
 export class ProductsService {
   constructor(private productsRepo: ProductsRepo) {}
+
+  async findProductById(id: string) {
+    return this.productsRepo.findOneById(id);
+  }
 }
--- src/domain/repos/products.repo.ts
+++ src/domain/repos/products.repo.ts
@@ -4,4 +4,8 @@ import { PrismaService } from 'libs/prisma/prisma.service';
 @Injectable()
 export class ProductsRepo {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findOneById(id: string) {
+    return this.prisma.product.findUnique({ where: { id } });
+  }
 }
