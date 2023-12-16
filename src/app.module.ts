f55c7354af9e403b6ede11fe9c29775788176619
tantality <angel.tretyak@mail.ru>
2023-12-17 00:27:20 +0300

    refactor: change a module name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'app/auth/auth.module';
 import { OrdersModule } from 'app/orders/orders.module';
-import { ProductsModule } from 'app/poducts/products.module';
+import { ProductsModule } from 'app/products/products.module';
 
 @Module({
   imports: [AuthModule, ProductsModule, OrdersModule],
