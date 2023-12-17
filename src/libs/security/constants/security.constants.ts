604fc41e8597d8e3ea50fc6d3c1453afc7ea3b1d
tantality <angel.tretyak@mail.ru>
2023-12-17 15:15:07 +0300

    feat: change an access token lifetime

--- src/libs/security/constants/security.constants.ts
+++ src/libs/security/constants/security.constants.ts
@@ -1,4 +1,4 @@
-export const ACCESS_TOKEN_LIFETIME_IN_MS = 30 * 60 * 1000 * 60; // 30 minutes
+export const ACCESS_TOKEN_LIFETIME_IN_MS = 60 * 1000 * 60; // 1 hour
 export const REFRESH_TOKEN_LIFETIME_IN_SEC = 2 * 30 * 24 * 60 * 60; // 60 days
 export const REFRESH_TOKEN_LIFETIME_IN_MS =
   REFRESH_TOKEN_LIFETIME_IN_SEC * 1000; // 60 days
