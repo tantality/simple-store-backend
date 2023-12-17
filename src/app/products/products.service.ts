deba57ea288fa807f81083d3565a8d2428793b68
tantality <angel.tretyak@mail.ru>
2023-12-17 20:03:14 +0300

    feat: replace types

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsRepo } from 'domain/repos/products.repo';
+import { ProductIdentifier } from 'types/model-identifiers.types';
 
 @Injectable()
 export class ProductsService {
   constructor(private productsRepo: ProductsRepo) {}
 
-  async findProductById(id: string) {
+  async findProductById(id: ProductIdentifier) {
     return this.productsRepo.findOneById(id);
   }
 }
--- src/domain/repos/products.repo.ts
+++ src/domain/repos/products.repo.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'libs/prisma/prisma.service';
+import { ProductIdentifier } from 'types/model-identifiers.types';
 
 @Injectable()
 export class ProductsRepo {
   constructor(private readonly prisma: PrismaService) {}
 
-  async findOneById(id: string) {
+  async findOneById(id: ProductIdentifier) {
     return this.prisma.product.findUnique({ where: { id } });
   }
 }
