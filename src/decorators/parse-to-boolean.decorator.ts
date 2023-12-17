7f6222f103e32c9f152b79b1f933108fa624ee20
tantality <angel.tretyak@mail.ru>
2023-12-17 20:51:37 +0300

    refactor: make a decorator code cleaner

--- src/decorators/parse-to-boolean.decorator.ts
+++ src/decorators/parse-to-boolean.decorator.ts
@@ -1,7 +1,21 @@
 import { Transform } from 'class-transformer';
 
+enum BooleanValue {
+  True = 'true',
+  False = 'false',
+}
+
 export function ParseToBoolean(): PropertyDecorator {
   return Transform(({ obj, key }) => {
-    return obj[key] === 'true' ? true : obj[key] === 'false' ? false : obj[key];
+    const objKey = obj[key];
+
+    switch (objKey) {
+      case BooleanValue.True:
+        return true;
+      case BooleanValue.False:
+        return false;
+      default:
+        return objKey;
+    }
   });
 }
