e92bd552774cf0b5bf39532f93de972ef6c97472
tantality <angel.tretyak@mail.ru>
2023-12-23 00:30:16 +0300

    feat: return products together with their count according to filters

--- src/app/products/products.controller.ts
+++ src/app/products/products.controller.ts
@@ -3,6 +3,7 @@ import { Query } from '@nestjs/common';
 import { InternalServerErrorException } from '@nestjs/common';
 import { Controller, Param, Get } from '@nestjs/common';
 import { ProductDto } from 'domain/dto/product.dto';
+import { ProductsDto } from 'domain/dto/products.dto';
 import { ErrorMessage } from 'enums/error-message.enum';
 import { SkipAccessTokenCheck } from 'libs/security/decorators/skip-access-token-check.decorator';
 import { GetAllProductsQueryDto } from './domain/get-all-products-query.dto';
@@ -15,8 +16,8 @@ export class ProductsController {
   @Get()
   @SkipAccessTokenCheck()
   async getAllProducts(@Query() query: GetAllProductsQueryDto) {
-    const productEntities = await this.productsService.findAllProducts(query);
-    return ProductDto.fromEntities(productEntities);
+    const productsAndCount = await this.productsService.findAllProducts(query);
+    return ProductsDto.fromResponse(productsAndCount);
   }
 
   @Get(':id')
